d3f1c484eea56290574b4a448b45e254012c9412
KlestiKapaj <162467857+KlestiKapaj@users.noreply.github.com>
2025-03-30 19:58:16 +0200

    Updated site with new changes

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const navMenu = document.querySelector(".nav-menu");
+    const body = document.querySelector('body');
+  
+    navMenu.classList.toggle("active");
+    body.classList.toggle('active');
+  }
+  
+  document.body.addEventListener('click', function(event) {
+    if (event.target.classList.contains('active') && event.target.tagName === 'BODY') {
+      toggleMenu();
+    }
+  });
